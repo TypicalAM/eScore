9fc3cc524f2ed40c501d58846150e7f169fa86e0
TypicalAM <tpam_piaseczny@protonmail.com>
2024-09-28 16:55:04 +0200

    Added a scoring baseline

---
+++ backend/aggregator.py
@@ -0,0 +1,45 @@
+from typing import Sequence, Tuple
+
+import requests
+
+from factors.base import ScoringFactor
+
+
+class URLException(Exception):
+    pass
+
+
+def check_url(url: str) -> (bool, str):
+    try:
+        if not url.startswith(("http://", "https://")):
+            return False, "not 'http://' or 'https://'"
+
+        response = requests.head(url, timeout=5)
+        if response.status_code >= 200 and response.status_code < 400:
+            return True, f"URL is valid. Status Code: {response.status_code}"
+        else:
+            return False, f"head returned: {response.status_code}"
+
+    except requests.exceptions.RequestException as e:
+        return False, f"requests error: {str(e)}"
+
+
+class ScoreAggregator:
+    def __init__(self, debug: bool = True):
+        self.debug: bool = debug
+        self.factors: Sequence[Tuple[ScoringFactor, float]] = []
+
+    def add_factor(self, factor: ScoringFactor, weight: float) -> None:
+        if self.debug:
+            print(f"Adding scoring factor: {factor.__name__}")
+        self.factors.append((factor, weight))
+
+    def check_url(self, url: str) -> int:
+        valid, reason = check_url(url)
+        if not valid:
+            raise URLException(f"Invalid URL: {reason}")
+
+        aggregate = 0
+        for factor, weight in self.factors:
+            aggregate += weight * factor.score(url)
+        return aggregate
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello world")
+import os
+from http import HTTPStatus
+
+from flask import Flask, jsonify, request
+
+from aggregator import ScoreAggregator, URLException
+
+DEBUG = os.getenv('HACKYEAH2024_DEBUG', False) == 'True'
+HOST = os.environ.get("HACKYEAH2024_HOST", "0.0.0.0")
+PORT = os.environ.get("HACKYEAH2024_PORT", 5000)
+
+aggregator = ScoreAggregator()
+app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    main()
+@app.route("/check_url", methods=["POST"])
+def home():
+    data = request.get_json()
+    if "url" not in data:
+        return jsonify({"error": "No URL provided"}), HTTPStatus.BAD_REQUEST
+
+    url = data["url"]
+    try:
+        aggregator.check_url(url)
+    except URLException as exc:
+        if DEBUG:
+            print(f"URL exception occured: {str(exc)}")
+        return jsonify({"error": str(exc)}), HTTPStatus.BAD_REQUEST
+    except Exception as exc:
+        print(f"Random exception occured while scoring the url: {str(exc)}")
+        return jsonify({"error": "Unknown error"}), HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host=HOST, port=PORT)
