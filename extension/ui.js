dadd487c15d1e54bf7facf3521b45445b69bdfe1
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-09-29 08:12:51 +0200

    web page

--- extension/ui.js
+++ extension/ui.js
@@ -5,6 +5,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('received message: ', request.text);
     const obj = JSON.parse(request.text);
     document.getElementById('mainFactor').innerHTML = obj.total_score + '%';
+    if (obj.total_score > 70) {
+        document.getElementById('mainFactor').style.color = '#A1C84E';
+    } else if (obj.total_score > 40){
+        document.getElementById('mainFactor').style.color = '#F5EA30';
+    } else if (obj.total_score > 25){
+        document.getElementById('mainFactor').style.color = '#F4A449';
+    } else {
+        document.getElementById('mainFactor').style.color = '#EB4F50';
+    }
     document.getElementById('tech').innerHTML = obj.aggregators.tech_standards.score + '%';
     document.getElementById('tech_description').innerHTML = obj.aggregators.tech_standards.notes.join('</br>');
     document.getElementById('review').innerHTML = obj.aggregators.reviews.score + '%';
